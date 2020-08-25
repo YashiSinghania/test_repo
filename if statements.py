aaa823a070d3ab3f165869014e26862cc22e44c6
YashiSinghania <ysinghania01@gmail.com>
2020-08-25 20:34:44 +0530

    first file push

---
+++ if statements.py
@@ -0,0 +1,10 @@
+is_hot=False
+is_cold=False
+if is_hot:
+    print('it is a hot day')
+elif is_cold:
+    print("it is a cold day")
+else:
+    print("it is a lovely day")
+print('enjoy you day')
+
